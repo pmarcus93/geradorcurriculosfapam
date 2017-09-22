202887c6e8be89691894d967dbcd8f85ad2f3f0c
Marcus Pereira <pereiramarcus93@gmail.com>
2017-09-22 16:44:12 -0300

    Alterações 22/09

--- index.js
+++ index.js
@@ -1,40 +1,47 @@
 var app = new Vue({
-  el: '#app',
-  data: {
-      dadosBasicos: {
-          nome: '',
-          idade: '',
-          genero: '',
-          estadoCivil: ''
-      },
-      contato: {
-          email : '',
-          telefone: ''
-      },
-      endereco: {
-          rua: '',
-          numero: '',
-          complemento: '',
-          bairro: '',
-          cidade: ''
-      },
-      formacao: {
-          instituicao: '',
-          descricao: '',
-          dataIngresso: '',
-          dataConclusao: ''
-      },
-      experienciaProfissional: {
-          empresa: '',
-          cargo: '',
-          descricaoCargo: '',
-          dataEntrada: '',
-          dataSaida: ''
-      }
-  },
-  methods: {
-    printPage: function() {
-      window.print();
+    el: '#app',
+    data: {
+        dadosBasicos: {
+            nome: '',
+            idade: '',
+            genero: '',
+            estadoCivil: ''
+        },
+        contato: {
+            email : [""],
+            telefones: [{numero: ''}]
+        },
+        endereco: {
+            rua: '',
+            numero: '',
+            complemento: '',
+            bairro: '',
+            cidade: ''
+        },
+        formacoes: [{
+            instituicao: '',
+            descricao: '',
+            dataIngresso: '',
+            dataConclusao: ''
+        }]
+        ,
+        experienciaProfissional: {
+            empresa: '',
+            cargo: '',
+            descricaoCargo: '',
+            dataEntrada: '',
+            dataSaida: ''
+        }
+    },
+    methods: {
+        printPage: function() {
+            window.print();
+        },
+        addFormacao: function () {
+            this.formacoes.push({instituicao: '', descricao: '', dataIngresso: '', dataConclusao: ''});
+        },
+        removeFormacao: function(index) {
+            this.formacoes.splice(index,1);
+        }
     }
-  }
 });
